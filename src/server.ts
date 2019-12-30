018402f7067ba321ec5ee64d39114c0c019fc9a2
ywalakamar <barxwells@gmail.com>
2019-12-30 17:59:00 +0300

    chore(express-setup): create project structure - add middleware - add services - closes #3 - closes #4

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,16 @@
-const sum = (a: number, b: number) => {
-  return a + b;
-};
-setInterval(() => console.log(sum(2, 3)), 1000);
+import http from "http";
+import express from "express";
+import { applyMiddleWare, applyRoutes } from "./utils";
+import routes from "./services";
+import middleware from "./middleware";
+
+const router = express();
+applyMiddleWare(middleware, router);
+applyRoutes(routes, router);
+
+const { PORT = 3000 } = process.env;
+const server = http.createServer(router);
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
