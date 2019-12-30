bf070647f1601d12bfc5d1dc777d2d8ce01d78cd
ywalakamar <barxwells@gmail.com>
2019-12-30 18:50:03 +0300

    chore(setup-errors): create error handlers - closes #5

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,22 @@ import express from "express";
 import { applyMiddleWare, applyRoutes } from "./utils";
 import routes from "./services";
 import middleware from "./middleware";
+import errorHandlers from "./middleware/errorHandlers";
+
+process.on("uncaughtException", e => {
+  console.log(e);
+  process.exit(1);
+});
+
+process.on("unhandledRejection", e => {
+  console.log(e);
+  process.exit(1);
+});
 
 const router = express();
 applyMiddleWare(middleware, router);
 applyRoutes(routes, router);
+applyMiddleWare(errorHandlers, router);
 
 const { PORT = 3000 } = process.env;
 const server = http.createServer(router);
